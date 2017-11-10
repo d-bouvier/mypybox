019c53c92188be5c15c236a8078d0c8658f6b441
Damien Bouvier <bouvierdamien@gmail.com>
2017-11-10 18:14:27 +0100

    Add __init__.py file
    Signed-off-by: Damien Bouvier <bouvierdamien@gmail.com>

---
+++ mypybox/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Python toolboxes for math functions, personalized plots and other things.
+
+Notes
+-----
+@author: bouvier (bouvier@ircam.fr)
+         Damien Bouvier, IRCAM, Paris
+
+Last modified on 10 Nov. 2017
+Developed for Python 3.6.1
+"""
+
+__author__ = "Damien Bouvier"
+__maintainer__ = "Damien Bouvier"
+__version__ = "0.1"
+__author_email__ = 'bouvierdamien@gmail.fr'
+
+from . import mathbox
+from . import savebox
+from . import plotbox
+from . import utilities
+
+__all__ = ['mathbox', 'savebox', 'plotbox', 'utilities']
