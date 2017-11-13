51397789a728676de77e7c8f343c1e509aed1cc6
d-bouvier <bouvierdamien@gmail.com>
2017-11-13 10:05:10 +0100

    Correct __init__.py file
    Signed-off-by: d-bouvier <bouvierdamien@gmail.com>

--- mypybox/__init__.py
+++ mypybox/__init__.py
@@ -7,7 +7,7 @@ Notes
 @author: bouvier (bouvier@ircam.fr)
          Damien Bouvier, IRCAM, Paris
 
-Last modified on 10 Nov. 2017
+Last modified on 13 Nov. 2017
 Developed for Python 3.6.1
 """
 
@@ -16,9 +16,9 @@ __maintainer__ = "Damien Bouvier"
 __version__ = "0.1"
 __author_email__ = 'bouvierdamien@gmail.fr'
 
-from . import mathbox
-from . import savebox
-from . import plotbox
-from . import utilities
+# from . import mathbox
+# from . import savebox
+# from . import plotbox
+# from . import utilities
 
-__all__ = ['mathbox', 'savebox', 'plotbox', 'utilities']
+__all__ = [] # ['mathbox', 'savebox', 'plotbox', 'utilities']
